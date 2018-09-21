d2f72e802190d153c07c33031a068034b7228dba
ciorici <cioricip@gmail.com>
2018-09-21 11:07:41 +0200

    rename domain for translations

--- dialog/dialog.php
+++ dialog/dialog.php
@@ -15,20 +15,20 @@ $support_shortcodes = true;
 <div id="wpz-options-buttons" class="clear">
     <div class="alignleft">
 
-        <input type="button" id="wpz-btn-cancel" class="button" name="cancel" value="<?php _e('Cancel', 'wpzoom-shortcodes-plugin'); ?>" accesskey="C" />
+        <input type="button" id="wpz-btn-cancel" class="button" name="cancel" value="<?php _e('Cancel', 'wpzoom-shortcodes'); ?>" accesskey="C" />
 
     </div>
     <div class="alignright">
 
-        <input type="button" id="wpz-btn-preview" class="button" name="preview" value="<?php _e('Preview', 'wpzoom-shortcodes-plugin'); ?>" accesskey="P" />
-        <input type="button" id="wpz-btn-insert" class="button-primary" name="insert" value="<?php _e('Insert', 'wpzoom-shortcodes-plugin'); ?>" accesskey="I" />
+        <input type="button" id="wpz-btn-preview" class="button" name="preview" value="<?php _e('Preview', 'wpzoom-shortcodes'); ?>" accesskey="P" />
+        <input type="button" id="wpz-btn-insert" class="button-primary" name="insert" value="<?php _e('Insert', 'wpzoom-shortcodes'); ?>" accesskey="I" />
 
     </div>
     <div class="clear"></div><!--/.clear-->
 </div><!--/#wpz-options-buttons .clear-->
 
 <div id="wpz-options" class="alignleft">
-    <h3><?php echo __( 'Customize the Shortcode', 'wpzoom-shortcodes-plugin' ); ?></h3>
+    <h3><?php echo __( 'Customize the Shortcode', 'wpzoom-shortcodes' ); ?></h3>
 
     <table id="wpz-options-table">
     </table>
@@ -37,7 +37,7 @@ $support_shortcodes = true;
 
 <div id="wpz-preview" class="alignleft">
 
-    <h3><?php echo __( 'Preview', 'wpzoom-shortcodes-plugin' ); ?></h3>
+    <h3><?php echo __( 'Preview', 'wpzoom-shortcodes' ); ?></h3>
 
     <iframe id="wpz-preview-iframe" frameborder="0" style="width:100%;height:250px" scrolling="no"></iframe>
 
@@ -51,7 +51,7 @@ $support_shortcodes = true;
 <?php  }  else { ?>
 
 <div id="wpz-options-error">
-    <p><?php echo __( 'Your version of theme does not yet support shortcodes.', 'wpzoom-shortcodes-plugin' ); ?></p>
+    <p><?php echo __( 'Your version of theme does not yet support shortcodes.', 'wpzoom-shortcodes' ); ?></p>
 </div>
 
 <?php  } ?>
